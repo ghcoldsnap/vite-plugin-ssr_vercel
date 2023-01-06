24dddb99beff41ea03e4d076bd9fb9dba980b833
ghcoldsnap <107293554+ghcoldsnap@users.noreply.github.com>
2023-01-06 14:25:11 +0800

    Update index.page.vue

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -11,6 +11,7 @@
 import Counter from './Counter.vue'
 export const HOME_URL =
   import.meta.env.NEXT_PUBLIC_HOME_URL || 'https://mail3.me'
+ console.log(HOME_URL)
 const components = { Counter }
 export default { components }
 </script>
